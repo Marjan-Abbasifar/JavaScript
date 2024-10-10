29fadc9551285aae4715b2583f331046ec41227c
marjanabbasifar <marjan.abbasifar@gmail.com>
2024-10-10 15:29:55 +0330

    Creating The JavaScript file for the Body Mass Index(BMI) calculator project

---
+++ Practice/BMI-calculator-app/script.js
@@ -0,0 +1,35 @@
+function  updateHeightValue(value) {
+    document.getElementById("height-value").textContent=value
+    
+}
+
+function updateWeightValue(value){
+    document.getElementById("weight-value").textContent=value
+}
+
+function calculateBMI(){
+    const height=document.getElementById("height").value;
+    const weight=document.getElementById("weight").value;
+    const age=document.getElementById("age").value;
+    const gender= document.querySelector("input[name='gender']:checked");
+    if (!age || !gender){
+        alert("please fill the all fields");
+        return
+    }
+    const bmi= (weight/(height/100)**2).toFixed(1)
+    document.getElementById("bmi-result").textContent=bmi;
+
+    let category="";
+    if(bmi<18.5){
+        category= "Underweight"
+    } else if (bmi<24.9){
+        category="Normal";
+    }else if (bmi<29.9){
+        category="Overweight"
+    } else {
+        category="Obese"
+    }
+    document.getElementById("bmi-category").textContent=category;
+    
+
+}
